97399542c753ebefa525f965b69a887f58d5ca01
Ian <ian.hanson.r@gmail.com>
2023-08-26 16:59:28 +1000

    Add header, total cost, company name, and logo.

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
+# Loop for multiple file generation.
 for filepath in filepaths:
     filename = Path(filepath).stem
     invoice_nr, invoice_date = filename.split("-")
 
     pdf = fpdf.FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    # Invoice number
+    # Invoice number.
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=0, h=8, txt=f"Invoice # {invoice_nr}", ln=1)
-    # Invoice date
+    # Invoice date.
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=0, h=8, txt=f"Date: {invoice_date}", ln=1)
     pdf.cell(w=0, h=8, txt=" ", ln=1)
 
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
-    # Table headers
+    # Table headers.
     table_headers = df.columns
     table_headers = [header.replace("_", " ").title() for header in table_headers]
     pdf.set_font(family="Times", size=10, style="B")
@@ -32,7 +33,7 @@ for filepath in filepaths:
     pdf.cell(h=8, w=30, txt=table_headers[3], border=1)
     pdf.cell(h=8, w=30, txt=table_headers[4], border=1, ln=1)
 
-    # Table data
+    # Loop for repetitive table rows.
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
@@ -41,5 +42,28 @@ for filepath in filepaths:
         pdf.cell(h=8, w=35, txt=str(row['amount_purchased']), border=1)
         pdf.cell(h=8, w=30, txt=str(row['price_per_unit']), border=1)
         pdf.cell(h=8, w=30, txt=str(row['total_price']), border=1, ln=1)
+
+    # Total cost row.
+    total_sum = df['total_price'].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(h=8, w=30, txt=" ", border=1)
+    pdf.cell(h=8, w=70, txt=" ", border=1)
+    pdf.cell(h=8, w=35, txt=" ", border=1)
+    pdf.cell(h=8, w=30, txt=" ", border=1)
+    pdf.cell(h=8, w=30, txt=str(total_sum), border=1, ln=1)
+    pdf.cell(h=8, w=35, txt=" ", ln=1)
+    pdf.cell(h=8, w=35, txt=" ", ln=1)
+
+    # Total cost sentence.
+    pdf.set_font(family="Times", size=14, style="BU")
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(h=8, w=30, txt=f"The total cost is ${total_sum}", ln=1)
+
+    # Company name and logo.
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.cell(h=3, w=0, txt=" ", ln=1)
+    pdf.cell(h=8, w=33, txt="PythonHow.com")
+    pdf.image("pythonhow.png", w=8, h=8)
 
     pdf.output(f"PDFs/{filename}.pdf")
